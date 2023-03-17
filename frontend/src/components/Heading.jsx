4914c8b5fa7610871f18cb0b79344faf076f67ae
ParasRawat29 <parasrawat29pr@gmail.com>
2023-03-17 14:43:21 +0530

    heading updated

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-function Heading({ fontSize, shadow = false }) {
-  const styles = {
+function Heading({ fontSize, onClick, styles, shadow = false }) {
+  const stylesHandler = {
     fontSize,
     textShadow: shadow ? "0px 0px 114px #60f2f2" : "",
+    ...styles,
   };
 
   return (
-    <h1 className="peerheading" style={styles}>
+    <h1 className="peerheading" style={stylesHandler} onClick={() => onClick()}>
       PEER CODING
     </h1>
   );
