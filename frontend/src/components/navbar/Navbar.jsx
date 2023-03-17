97042a2782c62fc73c6cb9fb0fb67688b4961ff8
ParasRawat29 <parasrawat29pr@gmail.com>
2023-03-17 14:42:42 +0530

    navbar fixed

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,11 +4,21 @@ import SignedOutLinks from "./SignedOutLinks";
 import "../../styles/navbar.css";
 import Heading from "../Heading";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 function Navbar({ showLogo }) {
   const { authenticated } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   return (
     <div className="navbarWrapper">
-      <div className="logo">{showLogo && <Heading fontSize="1.5rem" />}</div>
+      <div className="logo">
+        {showLogo && (
+          <Heading
+            fontSize="1.5rem"
+            onClick={() => navigate("/")}
+            styles={{ cursor: "pointer" }}
+          />
+        )}
+      </div>
       <div className="linksList">
         {!authenticated ? <SignedOutLinks /> : <SignedInLinks />}
       </div>
